87744ebcf50f8489989ab4e2732f45b5fb0b4172
michaelvidal <lvidal910@gmail.com>
2017-07-10 10:30:19 -0500

    Updates in ui & routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,8 +2,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { MarketComponent } from "app/pages/market/market.component";
 
 export const routes: Routes = [
+    { path: 'grocery-redux', component: MarketComponent },
     // otherwise redirect to home
-    { path: '**', redirectTo: '/' }
+    { path: '**', redirectTo: '/grocery-redux' }
 ];
 
 export const AppRouting = RouterModule.forRoot(routes);
